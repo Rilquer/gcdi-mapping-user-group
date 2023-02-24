4cd7637bec1f5c3fa1fe1d039663cdc4f41a29bd
Rilquer <38020251+Rilquer@users.noreply.github.com>
2023-02-23 21:33:36 -0500

    Update 01_intro_vector_R.R

--- tutorials/01_intro_vector_R/01_intro_vector_R.R
+++ tutorials/01_intro_vector_R/01_intro_vector_R.R
@@ -1,19 +1,31 @@
 
-library("ggplot2")
-library("sf")
+# Install packages
+install.packages()
+install.packages('tidyverse')
+install.packages('sf')
+
+library(tidyverse)
+library(sf)
 
 states <- read_sf('data/shapefiles/us_state_boundaries/ne_110m_admin_1_states_provinces.shp')
 
-ggplot()+geom_sf(data = states)
+ggplot() + geom_sf(data = states)
 
-ggplot()+geom_sf(data = states)+theme_minimal()
+ggplot() + geom_sf(data = states) + theme_minimal()
 
 countries <- read_sf('data/shapefiles/world_countries/ne_110m_admin_0_countries.shp')
-ggplot()+geom_sf(data = countries)+geom_sf(data = states)+theme_minimal()
 
-ggplot()+geom_sf(data = countries)+geom_sf(data = states)+
+ggplot() + geom_sf(data = countries) + geom_sf(data = states) + theme_minimal()
+
+ggplot() + geom_sf(data = countries) + theme_minimal()
+
+ggplot() + geom_sf(data = states) + geom_sf(data = countries, alpha = 0) +
+  theme_minimal()
+
+ggplot() + geom_sf(data = countries) + geom_sf(data = states) +
   coord_sf(xlim = c(-130.327727,-50.774995), ylim = c(24.321161,52.537236))+
   theme_minimal()
+
 
 library(tidyverse)
 states_gdp <- read_csv('data/states_gdp.csv')
@@ -21,12 +33,13 @@ states_gdp
 
 states <- states %>% left_join(states_gdp, by='name')
 
-ggplot()+geom_sf(data = countries)+geom_sf(data = states,aes(fill = gdp))+
-  coord_sf(xlim = c(-130.327727,-50.774995), ylim = c(24.321161,52.537236))+
+ggplot() + geom_sf(data = countries) +
+  geom_sf(data = states, aes(fill = gdp)) +
+  coord_sf(xlim = c(-130.327727,-50.774995), ylim = c(24.321161,52.537236)) +
   theme_minimal()
 
 # Customizing background color
-ggplot()+geom_sf(data = countries, fill = 'antiquewhite')+
+ggplot() + geom_sf(data = countries, fill = 'antiquewhite')+
   geom_sf(data = states,aes(fill = gdp))+
   coord_sf(xlim = c(-130.327727,-50.774995), ylim = c(24.321161,52.537236))+
   theme_bw()
@@ -35,6 +48,7 @@ ggplot()+geom_sf(data = countries, fill = 'antiquewhite')+
   geom_sf(data = states,aes(fill = gdp))+
   coord_sf(xlim = c(-130.327727,-50.774995), ylim = c(24.321161,52.537236))+
   theme_bw()+scale_fill_gradient2(low = '#f0f9e8',mid = '#7bccc4',high = '#0868ac')
+
 
 ggplot()+geom_sf(data = countries, fill = 'antiquewhite')+
   geom_sf(data = states,aes(fill = gdp))+
